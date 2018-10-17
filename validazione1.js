9a78cab98e4691eea76de32feab4ec79872855d5
lombardorobertowork <43878482+lombardorobertowork@users.noreply.github.com>
2018-10-17 11:32:32 +0200

    Update validazione1.js

--- validazione1.js
+++ validazione1.js
@@ -463,6 +463,6 @@ validaTutti( ["cap"], controllaCAP );
 validaTutti( ["partita iva", "PIVA"], controllaPIVA );
 validaTutti( ["quota"], controllaQUOTA );
 
-console.log("TEST ME 5u3");
+console.log("TEST ME 5u4");
 
 
